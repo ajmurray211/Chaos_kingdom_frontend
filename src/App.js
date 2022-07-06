8e691ca14bd9bef12af8cae38c059462eff0e756
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-07-06 11:33:32 -0400

    setup hom page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1> Welcome to the kingdom of chaos!!!</h1>
+      <a href='https://github.com/ajmurray211/Chaos_Kingdom/archive/refs/tags/V1.zip'
+      >Click here to downlaod the game</a>
+
+      <h1>Game play screenshots</h1>
+      <h2>title screen</h2>
+      <img src='./gamePics/title.png'></img>
+      <h2>Game board</h2>
+      <img src='./gamePics/board.png'></img>
+      <h2>Battle Map</h2>
+      <img src='./gamePics/battle.png'></img>
     </div>
   );
 }
